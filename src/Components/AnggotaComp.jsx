e48d4e38c2ec98db899c5bea4f0a80c691371750
Ilham Trihandoyo <114585143+Tri69@users.noreply.github.com>
2024-11-03 07:04:58 +0700

    Update AnggotaComp.jsx

--- src/Components/AnggotaComp.jsx
+++ src/Components/AnggotaComp.jsx
@@ -1,46 +1,20 @@
 import { useState, useEffect } from "react";
 import Table from 'react-bootstrap/Table';
-import Container from 'react-bootstrap/Container'
-import Form from 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 
-const AnggotaComp = ()=> {
-    const [dataSiswa, setDataSiswa] = useState([]);
-    const [dataSearch, setDataSearch] = useState('')
-    
-    
-    const FetchData = async () => {
-        const dataapi = await fetch(`https://api-server-five-beta.vercel.app/api/v2/`);
-        const result = await dataapi.json();
-        setDataSiswa(result);
-    }
+const IndexSearch = (props)=>{
+    const [dataSiswaSearch, setDataSiswaSearch] = useState([]);
     const FetchDataSearch = async ()=> {
-        const api = await fetch(`https://api-server-five-beta.vercel.app/api/v2/`+dataSearch)
-        const re = await api.json()
-        setDataSiswa(re)
+        const apis = await fetch(`https://api-server-five-beta.vercel.app/api/v2/${props.KeyWord}`)
+        const res = await apis.json()
+        setDataSiswaSearch(res)
     }
-    
-    useEffect(()=>{
-        if(dataSearch.length < 1){
-            FetchData()
-        }else{
-            FetchDataSearch()
-        }
+    useEffect(()=> {
+        FetchDataSearch()
     }, [])
-    const CariData = (e)=> {
-        let KeyWord = e.target.value;
-        setDataSearch(KeyWord);
-    }
-    console.log(dataSearch.length)
-    
     return(
         <>
-        <Container>
-            <div>
-                <Form.Control 
-                    type="text" 
-                    placeholder="name@example.com" 
-                    onChange={CariData} />
-            </div><br/>
             <Table responsive  variant="light">
       <thead>
         <tr>
@@ -53,9 +27,7 @@ const AnggotaComp = ()=> {
         </tr>
       </thead>
       <tbody>
-          {dataSiswa.map((datas)=> (
-          
-          
+          {dataSiswaSearch.map((datas)=> ( 
         <tr key={datas.id+1}>
           <td>{datas.id}</td>
           <td>{datas.name}</td>
@@ -67,6 +39,77 @@ const AnggotaComp = ()=> {
              ))}
      </tbody>
         </Table>
+        </>
+    )
+}
+
+const Index = ()=>{
+     const [dataSiswa, setDataSiswa] = useState([]);
+    
+     const FetchData = async () => {
+        const dataapi = await fetch(`https://api-server-five-beta.vercel.app/api/v2/`);
+        const result = await dataapi.json();
+        setDataSiswa(result);
+    }
+    useEffect(()=>{
+        FetchData()
+    }, [])
+    return(
+    <Table responsive  variant="light">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>ClassName</th>
+          <th>NIS</th>
+            <th>Bidang</th>
+            <th>Info</th>
+        </tr>
+      </thead>
+      <tbody>
+          {dataSiswa.map((datas)=> (
+        <tr key={datas.id+1}>
+          <td>{datas.id}</td>
+          <td>{datas.name}</td>
+          <td>{datas.class}</td>
+          <td>{datas.nis}</td>
+        <td>Programming</td>
+                  <td><a href="">View Detail</a></td>
+        </tr>
+             ))}
+     </tbody>
+        </Table>
+        )
+}
+
+const AnggotaComp = ()=> {
+    //const [secSearch, setSecSearch] = useState([])
+    const [dataSearch, setDataSearch] = useState('')
+   
+    const TrueAndFalse = ()=> {
+       if(dataSearch.length <= 0) {
+           return <Index/>;
+     } else {
+         return <IndexSearch KeyWord={dataSearch}/>;
+      }
+ }
+    function CariData(e) {
+        let KeyWord = e.target.value;
+        setDataSearch(KeyWord);
+    }
+    //console.log(dataSiswa)
+    
+    return(
+        <>
+        <Container>
+            <div>
+                <Form.Control 
+                    type="text" 
+                    placeholder="name@example.com" 
+                    onChange={CariData} />
+                <button>Search</button>
+            </div><br/>
+            {TrueAndFalse()}
         </Container>
         </>
     )
