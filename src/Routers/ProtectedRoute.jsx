970e34fcfd57954a1e411625a95e598f2af05e0d
Ilham Trihandoyo <114585143+Tri69@users.noreply.github.com>
2025-03-26 15:53:01 +0700

    Update ProtectedRoute.jsx

--- src/Routers/ProtectedRoute.jsx
+++ src/Routers/ProtectedRoute.jsx
@@ -2,6 +2,6 @@ import { Outlet, Navigate } from "react-router-dom"
 
 const ProtectedRoute = ()=>{
     const conditionAuth = localStorage.getItem('token');
-    return conditionAuth.length <= 3  || conditionAuth == null ?  <Navigate  to="app/login" /> : <Outlet/>
+    return conditionAuth.length >= 10  || !conditionAuth? <Outlet/> : <Navigate  to="app/login" />
 }
 export default ProtectedRoute;
